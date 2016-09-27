c32bc35b28f76a060ead8354aee8e0c34968ce5a
oscarotero <oom@oscarotero.com>
2016-09-27 19:55:10 +0200

    required mindplay/middleman in dev to run tests

--- tests/BoilerplateTest.php
+++ tests/BoilerplateTest.php
@@ -4,13 +4,19 @@ namespace Middlewares\Tests;
 
 use Middlewares\Boilerplate;
 use Zend\Diactoros\ServerRequest;
+use Zend\Diactoros\Response;
+use mindplay\middleman\Dispatcher;
 
 class BoilerplateTest extends \PHPUnit_Framework_TestCase
 {
     public function testBoilerplate()
     {
-        $middleware = new Boilerplate();
-        $response = $middleware->process(new ServerRequest(), new Delegate());
+        $response = (new Dispatcher([
+            new Boilerplate(),
+            function () {
+                return new Response();
+            },
+        ]))->dispatch(new ServerRequest());
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
     }
